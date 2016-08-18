34e9ee7db857c88217533e7bd561ba23043e12ea
MonsteraDeliciosa <kaja.otrebska@gmail.com>
2016-08-18 22:07:37 +0200

    added window.location for success and error login attempt

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-$(document).foundation();
+'use strict';
+
+var app = {
+    init: function() {
+        this.foundation();
+        this.apiLogin();
+    },
+
+    foundation: function() {
+        $(document).foundation();
+    },
+
+    apiLogin: function() {
+        $('#password').on('blur', function() {
+            console.log($(this).val());
+        });
+
+        $('.cta-btn').on('click', function() {
+            $.ajax({
+                type: "post",
+                data: {
+                    login: "efi",
+                    password: $('#password').val()
+                },
+                url: "https://efigence-camp.herokuapp.com/api/login",
+                error: function(response) {
+                    window.location = "http://nooooooooooooooo.com";
+                    console.log(response.responseText);
+                    console.log('error!!!!');
+                },
+                success: function(response) {
+                    window.location = "http://www.nyan.cat";
+                    console.log(response);
+                    console.log('success!!!!');
+                }
+            });
+        });
+    }
+
+};
+
+$(document).ready(function() {
+    app.init();
+});
